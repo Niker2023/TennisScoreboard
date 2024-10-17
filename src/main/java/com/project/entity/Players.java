b74f284c3dc58376fb17259a3c092df978e4c3c2
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-10-17 15:13:46 +0300

    added players entity

---
+++ src/main/java/com/project/entity/Players.java
@@ -0,0 +1,22 @@
+package com.project.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Players {
+
+    @Id
+    private int id;
+    private String player;
+
+}
+
