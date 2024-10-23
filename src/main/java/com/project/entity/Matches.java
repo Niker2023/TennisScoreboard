133cb0ecfedef98661533a64fe877884fe201482
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-10-23 18:09:39 +0300

    added entity fields

--- src/main/java/com/project/entity/Matches.java
+++ src/main/java/com/project/entity/Matches.java
@@ -1,4 +1,19 @@
 package com.project.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Matches {
+
+    @Id
+    private int id;
 }
