bf6851cbb1c686b49e387625702b4a433283640e
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-10-23 18:07:46 +0300

    added if not exist and matches table

--- src/main/resources/h2init.sql
+++ src/main/resources/h2init.sql
@@ -1,3 +1,16 @@
-CREATE SCHEMA tennisScoreboard;
+CREATE SCHEMA if not exists tennisScoreboard;
 SET SCHEMA tennisScoreboard;
-CREATE TABLE players (id INT2 PRIMARY KEY AUTO_INCREMENT, player VARCHAR(100) NOT NULL);
+
+CREATE TABLE if not exists players (
+    id INT2 PRIMARY KEY AUTO_INCREMENT,
+    player VARCHAR(100) NOT NULL);
+
+CREATE TABLE if not exists matches (
+    id INT2 PRIMARY KEY AUTO_INCREMENT,
+    player1 int2,
+    player2 int2,
+    winner int2,
+    foreign key (player1) references players(id),
+    foreign key (player2) references players(id),
+    foreign key (winner) references players(id)
+);
