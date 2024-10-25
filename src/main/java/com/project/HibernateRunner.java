3ea7cd5d1e7870ac988b6860039aa7a8f5781f4c
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-10-25 16:42:15 +0300

    added logger annotation

--- src/main/java/com/project/HibernateRunner.java
+++ src/main/java/com/project/HibernateRunner.java
@@ -4,11 +4,15 @@ package com.project;
 import com.project.entity.Matches;
 import com.project.entity.Players;
 import com.project.entity.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.SessionFactory;
 
+@Slf4j
 public class HibernateRunner {
 
     public static void main(String[] args) {
+
+        String sql = "select * from PLAYERS where player";
 
         Players players1 = Players.builder()
                 .player("Maradonna")
@@ -18,28 +22,37 @@ public class HibernateRunner {
                 .player("Mary")
                 .build();
 
+        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+             var session = sessionFactory.openSession()) {
+
+            session.beginTransaction();
+
+            session.persist(players1);
+            session.persist(players2);
+
+            session.getTransaction().commit();
+        }
+
         var matches1 = Matches.builder()
                 .player1(players1)
                 .player2(players2)
                 .winner(players1)
                 .build();
 
+        log.info(matches1.toString());
+        System.out.println(matches1.toString());
+
         try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
              var session = sessionFactory.openSession()) {
-
             session.beginTransaction();
 
+//            session.refresh(players1);
+//            session.refresh(players2);
 
-            session.save(players1);
-            session.save(players2);
-
-//            session.flush();
 
             session.persist(matches1);
-//            var players = session.get(Players.class, 1);
 
             session.getTransaction().commit();
-//            System.out.println(players);
         }
     }
 }
