23ba0013a0f611ba5a08c29acf26d86de3c15843
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-11-02 17:45:37 +0300

    changed methods save to persist

--- src/main/java/com/project/HibernateRunner.java
+++ src/main/java/com/project/HibernateRunner.java
@@ -12,8 +12,6 @@ public class HibernateRunner {
 
     public static void main(String[] args) {
 
-        String sql = "select * from PLAYERS where player";
-
         Players players1 = Players.builder()
                 .player("Maradonna")
                 .build();
@@ -30,25 +28,16 @@ public class HibernateRunner {
             session.persist(players1);
             session.persist(players2);
 
-            session.getTransaction().commit();
-        }
+            var matches1 = Matches.builder()
+                    .player1(players1)
+                    .player2(players2)
+                    .winner(players1)
+                    .build();
 
-        var matches1 = Matches.builder()
-                .player1(players1)
-                .player2(players2)
-                .winner(players1)
-                .build();
+            log.info(matches1.toString());
+            System.out.println(matches1);
 
-        log.info(matches1.toString());
-        System.out.println(matches1.toString());
-
-        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-             var session = sessionFactory.openSession()) {
-            session.beginTransaction();
-
-//            session.refresh(players1);
-//            session.refresh(players2);
-
+            session.flush();
 
             session.persist(matches1);
 
