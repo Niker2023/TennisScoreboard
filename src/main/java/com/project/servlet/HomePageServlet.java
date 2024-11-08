5ca1977f6026fbd4cb9f00d2ede2561df07ff586
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-11-08 18:01:09 +0300

    added servlet handler url /home-page

---
+++ src/main/java/com/project/servlet/HomePageServlet.java
@@ -0,0 +1,20 @@
+package com.project.servlet;
+
+import java.io.*;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/home-page")
+public class HomePageServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        request.setAttribute("new_match_url", "new-match");
+        request.setAttribute("matches_url", "matches");
+
+        request.getRequestDispatcher("/WEB-INF/home-page.jsp")
+                .forward(request, response);
+    }
+}
