03953084695c4b636929791bb66bf60af7406e1f
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-11-18 17:59:33 +0300

    add save method

--- src/main/java/com/project/dao/MatchDao.java
+++ src/main/java/com/project/dao/MatchDao.java
@@ -1,4 +1,23 @@
 package com.project.dao;
 
+import com.project.entity.Matches;
+import com.project.util.HibernateUtil;
+import lombok.NoArgsConstructor;
+import org.hibernate.Session;
+
+@NoArgsConstructor
 public class MatchDao {
+
+    public static final MatchDao INSTANCE = new MatchDao();
+
+    public static MatchDao getInstance() {
+        return INSTANCE;
+    }
+
+    public void save(Matches match) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        session.persist(match);
+        session.getTransaction().commit();
+    }
 }
