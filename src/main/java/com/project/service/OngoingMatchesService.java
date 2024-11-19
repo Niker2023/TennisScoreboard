00739160b964d6e350b0ac353a6c8aa4e4e597a5
Niker2023 <berserg@bk.ru>
2024-11-19 23:44:24 +0300

    added CRD methods

--- src/main/java/com/project/service/OngoingMatchesService.java
+++ src/main/java/com/project/service/OngoingMatchesService.java
@@ -1,7 +1,22 @@
 package com.project.service;
 
+import com.project.entity.MatchScore;
+
+import java.util.*;
+
 public class OngoingMatchesService {
 
+    private static final Map<UUID, MatchScore> matches = new HashMap<>();
 
+    public static void addMatch(UUID uuid, MatchScore matchScore) {
+        matches.put(uuid, matchScore);
+    }
 
+    public static MatchScore getMatch(UUID uuid) {
+        return matches.get(uuid);
+    }
+
+    public static void removeMatch(UUID uuid) {
+        matches.remove(uuid);
+    }
 }
