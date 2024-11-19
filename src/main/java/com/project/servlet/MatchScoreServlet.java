67722a8dcf0543f7f82fc4405220f7e2a71db203
Niker2023 <berserg@bk.ru>
2024-11-19 23:44:50 +0300

    changed redirect

--- src/main/java/com/project/servlet/MatchScoreServlet.java
+++ src/main/java/com/project/servlet/MatchScoreServlet.java
@@ -1,5 +1,9 @@
 package com.project.servlet;
 
+import com.project.dto.PlayerDto;
+import com.project.entity.MatchScore;
+import com.project.entity.Players;
+import com.project.service.OngoingMatchesService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,8 +17,11 @@ public class MatchScoreServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.setAttribute("player1", "new-match");
-        request.setAttribute("player2", "matches");
+        var player1 = request.getParameter("player1");
+        var player2 = request.getParameter("player2");
+
+        request.setAttribute("player1", "%s".formatted(player1));
+        request.setAttribute("player2", "%s".formatted(player2));
 
         request.getRequestDispatcher("/WEB-INF/match-score.jsp")
                 .forward(request, response);
