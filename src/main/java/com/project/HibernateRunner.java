e6ce45c0d9923208e6d557140470abb29cf2ed9c
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-11-20 18:04:48 +0300

    commented

--- src/main/java/com/project/HibernateRunner.java
+++ src/main/java/com/project/HibernateRunner.java
@@ -12,36 +12,36 @@ public class HibernateRunner {
 
     public static void main(String[] args) {
 
-        Players players1 = Players.builder()
-                .player("Maradonna")
-                .build();
+//        Players players1 = Players.builder()
+//                .player("Maradonna")
+//                .build();
+//
+//        Players players2 = Players.builder()
+//                .player("Mary")
+//                .build();
 
-        Players players2 = Players.builder()
-                .player("Mary")
-                .build();
-
-        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-             var session = sessionFactory.openSession()) {
-
-            session.beginTransaction();
-
-            session.persist(players1);
-            session.persist(players2);
-
-            var matches1 = Matches.builder()
-                    .player1(players1)
-                    .player2(players2)
-                    .winner(players1)
-                    .build();
-
-            log.info(matches1.toString());
-            System.out.println(matches1);
-
-            session.flush();
-
-            session.persist(matches1);
-
-            session.getTransaction().commit();
-        }
+//        try (SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+//             var session = sessionFactory.openSession()) {
+//
+//            session.beginTransaction();
+//
+//            session.persist(players1);
+//            session.persist(players2);
+//
+//            var matches1 = Matches.builder()
+//                    .player1(players1)
+//                    .player2(players2)
+//                    .winner(players1)
+//                    .build();
+//
+//            log.info(matches1.toString());
+//            System.out.println(matches1);
+//
+//            session.flush();
+//
+//            session.persist(matches1);
+//
+//            session.getTransaction().commit();
+//        }
     }
 }
