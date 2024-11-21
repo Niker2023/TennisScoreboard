310f12cd8ea4eb2ab5bc01afc88be9af899102e8
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2024-11-21 17:10:47 +0300

    changed logic to new matchScore

--- src/main/java/com/project/util/MatchScoreViewUtil.java
+++ src/main/java/com/project/util/MatchScoreViewUtil.java
@@ -4,11 +4,9 @@ import com.project.dao.PlayerDao;
 import com.project.entity.MatchScore;
 import jakarta.servlet.http.HttpServletRequest;
 
-import java.util.UUID;
-
 public class MatchScoreViewUtil {
 
-    public static HttpServletRequest getFilledRequest (HttpServletRequest req, UUID uuid, MatchScore match) {
+    public static HttpServletRequest getFilledRequest (HttpServletRequest req, MatchScore match) {
 
         Integer player1Id = match.getPlayer1Id();
         Integer player2Id = match.getPlayer2Id();
@@ -16,31 +14,29 @@ public class MatchScoreViewUtil {
         String player1 = PlayerDao.getInstance().getNameById(player1Id);
         String player2 = PlayerDao.getInstance().getNameById(player2Id);
 
-        req.setAttribute("UUID", "%s".formatted(uuid));
+        req.setAttribute(Player.ONE.toString(), "%s".formatted(player1));
+        req.setAttribute(Player.TWO.toString(), "%s".formatted(player2));
 
-        req.setAttribute("player1", "%s".formatted(player1));
-        req.setAttribute("player2", "%s".formatted(player2));
-
-        Integer set1ScorePlayer1 = match.getSet1().get(player1Id);
-        Integer set1ScorePlayer2 = match.getSet1().get(player2Id);
+        Integer set1ScorePlayer1 = match.getSet1Score(Player.ONE.toString());
+        Integer set1ScorePlayer2 = match.getSet1Score(Player.TWO.toString());
 
         req.setAttribute("set1ScorePlayer1", "%s".formatted(set1ScorePlayer1));
         req.setAttribute("set1ScorePlayer2", "%s".formatted(set1ScorePlayer2));
 
-        var set2ScorePlayer1 = match.getSet2().get(player1Id);
-        var set2ScorePlayer2 = match.getSet2().get(player2Id);
+        var set2ScorePlayer1 = match.getSet2Score(Player.ONE.toString());
+        var set2ScorePlayer2 = match.getSet2Score(Player.TWO.toString());
 
         req.setAttribute("set2ScorePlayer1", "%s".formatted(set2ScorePlayer1));
         req.setAttribute("set2ScorePlayer2", "%s".formatted(set2ScorePlayer2));
 
-        var set3ScorePlayer1 = match.getSet3().get(player1Id);
-        var set3ScorePlayer2 = match.getSet3().get(player2Id);
+        var set3ScorePlayer1 = match.getSet3Score(Player.ONE.toString());
+        var set3ScorePlayer2 = match.getSet3Score(Player.TWO.toString());
 
         req.setAttribute("set3ScorePlayer1", "%s".formatted(set3ScorePlayer1));
         req.setAttribute("set3ScorePlayer2", "%s".formatted(set3ScorePlayer2));
 
-        var pointsScorePlayer1 = match.getPoints().get(player1Id);
-        var pointsScorePlayer2 = match.getPoints().get(player2Id);
+        var pointsScorePlayer1 = match.getPoints(Player.ONE.toString());
+        var pointsScorePlayer2 = match.getPoints(Player.TWO.toString());
 
         if (pointsScorePlayer1 < 4 && pointsScorePlayer2 < 4) {
             req.setAttribute("pointsScorePlayer2", "%s".formatted(pointsView(pointsScorePlayer2)));
