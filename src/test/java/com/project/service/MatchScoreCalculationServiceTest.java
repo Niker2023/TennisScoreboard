0e3d3832a4742adaa2eb6304f4d29dcb21b05370
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-03 17:53:48 +0300

    added test cases

--- src/test/java/com/project/service/MatchScoreCalculationServiceTest.java
+++ src/test/java/com/project/service/MatchScoreCalculationServiceTest.java
@@ -7,31 +7,131 @@ import static org.junit.jupiter.api.Assertions.*;
 class MatchScoreCalculationServiceTest {
 
     @Test
-    void calculateWinPointPlayer1() {
-        var testMatch = new MatchScore(0, 1);
+    void whenPlayer1WinsPoint_thenSuccess() {
+        var testMatch = new MatchScore(1, 2);
         testMatch.setPoints("player1",3);
         testMatch.setPoints("player2",3);
         MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
 
-        var expectedMatchResult = new MatchScore(0, 1);
+        var expectedMatchResult = new MatchScore(1, 2);
         expectedMatchResult.setPoints("player1",4);
         expectedMatchResult.setPoints("player2",3);
+
         assertEquals(testMatch, expectedMatchResult);
     }
 
-
     @Test
-    void calculateWinSetPlayer2() {
+    void whenPlayer2WinsPointAndGameWithFirstAD_thenSuccess() {
         var testMatch = new MatchScore(0, 1);
         testMatch.setPoints("player1",3);
         testMatch.setPoints("player2",4);
         MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
-        
+
         var expectedMatchResult = new MatchScore(0, 1);
         expectedMatchResult.setPoints("player1",0);
         expectedMatchResult.setPoints("player2",0);
-        expectedMatchResult.setSet1Score("player1", 0);
         expectedMatchResult.setSet1Score("player2", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer1WinsPointAndGameWith_40_0_thenSuccess() {
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",0);
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setSet1Score("player1", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer2WinsPointAndGameWithMoreThen4PointsAndAD_thenSuccess() {
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",5);
+        testMatch.setPoints("player2",6);
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player2", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer1WinsPointAndGameAndSet1_thenSuccess() {
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",2);
+        testMatch.setSet1Score("player1", 5);
+        testMatch.setSet1Score("player2", 4);
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 6);
+        expectedMatchResult.setSet1Score("player2", 4);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer2WinsPointAndGameAndSet1AndGameInSet2_thenSuccess() {
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player2",3);
+        testMatch.setPoints("player1",2);
+        testMatch.setSet1Score("player2", 5);
+        testMatch.setSet1Score("player1", 4);
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 4);
+        expectedMatchResult.setSet1Score("player2", 6);
+        expectedMatchResult.setSet2Score("player2", 1);
+        expectedMatchResult.setSet2Score("player1", 0);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer1WinsPointAndGameAndSet2AndGameInSet3_thenSuccess() {
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",2);
+        testMatch.setSet1Score("player1", 7);
+        testMatch.setSet1Score("player2", 6);
+        testMatch.setSet2Score("player1", 5);
+        testMatch.setSet2Score("player2", 4);
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setSet1Score("player2", 6);
+        expectedMatchResult.setSet1Score("player1", 7);
+        expectedMatchResult.setSet2Score("player1", 6);
+        expectedMatchResult.setSet2Score("player2", 4);
+        expectedMatchResult.setSet3Score("player1", 1);
+
         assertEquals(testMatch, expectedMatchResult);
     }
 }
