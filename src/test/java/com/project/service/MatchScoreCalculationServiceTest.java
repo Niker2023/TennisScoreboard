4d0a5d63434bf8804b7eb675a696306d0af639cd
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-04 17:36:21 +0300

    added tests for wins in tiebreak

--- src/test/java/com/project/service/MatchScoreCalculationServiceTest.java
+++ src/test/java/com/project/service/MatchScoreCalculationServiceTest.java
@@ -116,12 +116,9 @@ class MatchScoreCalculationServiceTest {
         testMatch.setSet1Score("player2", 6);
         testMatch.setSet2Score("player1", 5);
         testMatch.setSet2Score("player2", 4);
-        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
-        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
-        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
-        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
-        MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
-
+        for (int i = 0; i < 5; i++) {
+            MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        }
 
         var expectedMatchResult = new MatchScore(1, 2);
         expectedMatchResult.setPoints("player2",0);
@@ -131,6 +128,79 @@ class MatchScoreCalculationServiceTest {
         expectedMatchResult.setSet2Score("player1", 6);
         expectedMatchResult.setSet2Score("player2", 4);
         expectedMatchResult.setSet3Score("player1", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenScoreInSet1_5_6_AndPlayer1WinsGame_thenTiebreak_thenSuccess() {
+        new MatchScore(1, 2);
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",0);
+        testMatch.setPoints("player2",0);
+        testMatch.setSet1Score("player1", 5);
+        testMatch.setSet1Score("player2", 6);
+        for (int i = 0; i < 9; i++) {
+            MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        }
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",5);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 6);
+        expectedMatchResult.setSet1Score("player2", 6);
+
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenTiebreakAndPlayer1Wins7ScoreInSet2_thenStartSet3_thenSuccess() {
+        new MatchScore(1, 2);
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",0);
+        testMatch.setPoints("player2",0);
+        testMatch.setSet1Score("player1", 5);
+        testMatch.setSet1Score("player2", 7);
+        testMatch.setSet2Score("player1", 6);
+        testMatch.setSet2Score("player2", 6);
+        for (int i = 0; i < 11; i++) {
+            MatchScoreCalculationService.changeMatchScore(testMatch, "player1");
+        }
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 5);
+        expectedMatchResult.setSet1Score("player2", 7);
+        expectedMatchResult.setSet2Score("player1", 7);
+        expectedMatchResult.setSet2Score("player2", 6);
+        expectedMatchResult.setSet3Score("player1", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenTiebreakAndPlayer2Wins2PointsWhen_7_7_thenPlayer2WinsTiebreak_thenSuccess() {
+        new MatchScore(1, 2);
+        var testMatch = new MatchScore(1, 2);
+        testMatch.setPoints("player1",7);
+        testMatch.setPoints("player2",7);
+        testMatch.setSet1Score("player1", 7);
+        testMatch.setSet1Score("player2", 5);
+        testMatch.setSet2Score("player1", 6);
+        testMatch.setSet2Score("player2", 6);
+        for (int i = 0; i < 4; i++) {
+            MatchScoreCalculationService.changeMatchScore(testMatch, "player2");
+        }
+
+        var expectedMatchResult = new MatchScore(1, 2);
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",2);
+        expectedMatchResult.setSet1Score("player1", 7);
+        expectedMatchResult.setSet1Score("player2", 5);
+        expectedMatchResult.setSet2Score("player1", 6);
+        expectedMatchResult.setSet2Score("player2", 7);
 
         assertEquals(testMatch, expectedMatchResult);
     }
