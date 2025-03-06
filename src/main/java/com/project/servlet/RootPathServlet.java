f7726e1948d1a2e752f4df6dd079fa9d2b3de34b
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-06 17:45:00 +0300

    added RootPathServlet to redirect on NewMatchServlet

---
+++ src/main/java/com/project/servlet/RootPathServlet.java
@@ -0,0 +1,16 @@
+package com.project.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/")
+public class RootPathServlet extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("/new-match");
+    }
+}
