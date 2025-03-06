cd92fb5d8e819de2b2fe864058d1c4ba8e4c2b9b
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-06 17:18:38 +0300

    added logic for view score on breakpoint and tiebreak

--- src/main/java/com/project/util/MatchScoreViewUtil.java
+++ src/main/java/com/project/util/MatchScoreViewUtil.java
@@ -35,28 +35,43 @@ public class MatchScoreViewUtil {
         req.setAttribute("set3ScorePlayer1", "%s".formatted(set3ScorePlayer1));
         req.setAttribute("set3ScorePlayer2", "%s".formatted(set3ScorePlayer2));
 
-        var pointsScorePlayer1 = match.getPoints(Player.ONE.toString());
-        var pointsScorePlayer2 = match.getPoints(Player.TWO.toString());
-
-        if (pointsScorePlayer1 < 4 && pointsScorePlayer2 < 4) {
-            req.setAttribute("pointsScorePlayer2", "%s".formatted(pointsView(pointsScorePlayer2)));
-            req.setAttribute("pointsScorePlayer1", "%s".formatted(pointsView(pointsScorePlayer1)));
-        }
+        req.setAttribute("pointsScorePlayer1", "%s".formatted(pointsView(match, Player.ONE.toString())));
+        req.setAttribute("pointsScorePlayer2", "%s".formatted(pointsView(match, Player.TWO.toString())));
 
         return req;
     }
 
 
-    private static String pointsView(Integer points) {
-
-        if (points == 1) {
+    private static String pointsView(MatchScore match, String player1) {
+        String player2;
+        if ("player1".equals(player1)) {
+            player2 = "player2";
+        } else {
+            player2 = "player1";
+        }
+        if (isTiebreak(match)) {
+            return match.getPoints(player1).toString();
+        } else if (match.getPoints(player1) > 2 && match.getPoints(player2) > 2) {
+            if (match.getPoints(player2) - match.getPoints(player1) == 1) {
+                return " ";
+            } else if (match.getPoints(player1) - match.getPoints(player2) == 1) {
+                return "AD";
+            }
+            return "40";
+        } else if (match.getPoints(player1) == 1) {
             return "15";
-        } else if (points == 2) {
+        } else if (match.getPoints(player1) == 2) {
             return "30";
-        } else if (points == 3) {
+        } else if (match.getPoints(player1) == 3) {
             return "40";
         }
         return "0";
     }
 
+
+    private static boolean isTiebreak(MatchScore match) {
+        return (match.getSet1Score("player1") == 6 && match.getSet1Score("player2") == 6)
+                || (match.getSet2Score("player1") == 6 && match.getSet2Score("player2") == 6)
+                || (match.getSet3Score("player1") == 6 && match.getSet3Score("player2") == 6);
+    }
 }
