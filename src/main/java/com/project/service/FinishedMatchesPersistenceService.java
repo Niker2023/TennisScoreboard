249dab497e1aed89bfed37a58ce80474d65e229f
Niker2023 <berserg@bk.ru>
2025-03-16 22:49:30 +0300

    added methods for interacting with dao layer

--- src/main/java/com/project/service/FinishedMatchesPersistenceService.java
+++ src/main/java/com/project/service/FinishedMatchesPersistenceService.java
@@ -1,4 +1,34 @@
 package com.project.service;
 
+import com.project.dao.MatchDao;
+import com.project.dao.PlayerDao;
+import com.project.entity.MatchScore;
+import com.project.entity.Matches;
+
+import java.util.List;
+
 public class FinishedMatchesPersistenceService {
+
+
+    private final MatchDao matchDao = MatchDao.getInstance();
+
+
+    private static final FinishedMatchesPersistenceService INSTANCE = new FinishedMatchesPersistenceService();
+
+
+    public static FinishedMatchesPersistenceService getInstance() { return INSTANCE; }
+
+
+    public void save(MatchScore matchScore) {
+        Matches match = new Matches();
+        match.setPlayer1(PlayerDao.getInstance().getPlayerById(matchScore.getPlayer1Id()));
+        match.setPlayer2(PlayerDao.getInstance().getPlayerById(matchScore.getPlayer2Id()));
+        match.setWinner(PlayerDao.getInstance().getPlayerById(matchScore.getWinnerId()));
+        matchDao.save(match);
+    }
+
+
+    public List<Matches> getMatches() {
+        return matchDao.getMatches();
+    }
 }
