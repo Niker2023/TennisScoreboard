e3613a752be2094bdc69a47d1a889225b1256dc9
Niker2023 <berserg@bk.ru>
2025-03-19 23:47:11 +0300

    feat: add import sql file to insert test data

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+INSERT INTO Players(name) VALUES ('D. Medvedev');
+INSERT INTO Players(name) VALUES ('B. Djokovic');
+INSERT INTO Players(name) VALUES ('B. Yeltsin');
+INSERT INTO Players(name) VALUES ('R. Nadal');
+
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,2,1);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (2,3,2);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (3,2,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,1);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (3,4,3);
