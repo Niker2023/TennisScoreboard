875d8dea6649715fc59ab51a9faf0936a5d7d365
Niker2023 <berserg@bk.ru>
2025-03-19 23:51:47 +0300

    feat: add the private static final variable sessionFactory and getter for it for one-time initialization

--- src/main/java/com/project/util/HibernateUtil.java
+++ src/main/java/com/project/util/HibernateUtil.java
@@ -1,11 +1,15 @@
 package com.project.util;
 
+import lombok.Getter;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    public static SessionFactory getSessionFactory() {
+    @Getter
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
         Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
         return configuration.buildSessionFactory();
