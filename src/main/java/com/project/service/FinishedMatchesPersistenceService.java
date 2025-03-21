9d54420aa95b1711d565d208aec62881153e9c7b
Niker2023 <berserg@bk.ru>
2025-03-21 14:29:59 +0300

    feat: add a method for recording a match to the datebase and a method for creating a match dto

--- src/main/java/com/project/service/FinishedMatchesPersistenceService.java
+++ src/main/java/com/project/service/FinishedMatchesPersistenceService.java
@@ -2,24 +2,18 @@ package com.project.service;
 
 import com.project.dao.MatchDao;
 import com.project.dao.PlayerDao;
+import com.project.dto.FinishedMatchDto;
 import com.project.entity.MatchScore;
 import com.project.entity.Matches;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class FinishedMatchesPersistenceService {
 
-
-    private final MatchDao matchDao = MatchDao.getInstance();
-
-
-    private static final FinishedMatchesPersistenceService INSTANCE = new FinishedMatchesPersistenceService();
-
-
-    public static FinishedMatchesPersistenceService getInstance() { return INSTANCE; }
-
-
     public void save(MatchScore matchScore) {
+        MatchDao matchDao = MatchDao.getInstance();
         Matches match = new Matches();
         match.setPlayer1(PlayerDao.getInstance().getPlayerById(matchScore.getPlayer1Id()));
         match.setPlayer2(PlayerDao.getInstance().getPlayerById(matchScore.getPlayer2Id()));
@@ -27,8 +21,23 @@ public class FinishedMatchesPersistenceService {
         matchDao.save(match);
     }
 
+    public List<FinishedMatchDto> getFinishedMatches() {
 
-    public List<Matches> getMatches() {
-        return matchDao.getMatches();
+        var matches = MatchDao.getInstance().getMatches();
+        List<FinishedMatchDto> finishedMatches = new ArrayList<>();
+
+        for (var match : matches) {
+            finishedMatches.add(toDto(match));
+        }
+        return finishedMatches;
+    }
+
+    private FinishedMatchDto toDto(Matches match) {
+
+        String player1Name = match.getPlayer1().getName();
+        String player2Name = match.getPlayer2().getName();
+        String winnerName = match.getWinner().getName();
+
+        return new FinishedMatchDto(player1Name, player2Name, winnerName);
     }
 }
