917b4ab6919c1e234007705669943ff59b2769dd
Niker2023 <berserg@bk.ru>
2025-03-22 21:11:53 +0300

    feat: add an attribute setting to check empty finished matches list and to handle the "NoResultException" exception

--- src/main/java/com/project/servlet/CompletedMatchesServlet.java
+++ src/main/java/com/project/servlet/CompletedMatchesServlet.java
@@ -1,6 +1,8 @@
 package com.project.servlet;
 
+import com.project.dto.FinishedMatchDto;
 import com.project.service.FinishedMatchesPersistenceService;
+import jakarta.persistence.PersistenceException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/matches")
 public class CompletedMatchesServlet extends HttpServlet {
@@ -15,9 +19,23 @@ public class CompletedMatchesServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        var matches = FinishedMatchesPersistenceService.getInstance().getMatches();
+        var filterByPlayerName = request.getParameter("playerName");
 
-        request.setAttribute("matches", matches);
+        FinishedMatchesPersistenceService finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+
+        List<FinishedMatchDto> finishedMatches = new ArrayList<>();
+
+        if (filterByPlayerName == null || filterByPlayerName.isBlank()) {
+            finishedMatches = finishedMatchesPersistenceService.getFinishedMatches();
+        } else {
+            try {
+                finishedMatches = finishedMatchesPersistenceService.getFinishedMatchesByPlayerName(filterByPlayerName);
+            } catch (PersistenceException e) {
+                request.setAttribute("NoResultException", true);
+            }
+        }
+
+        request.setAttribute("matches", finishedMatches);
 
         request.getRequestDispatcher("/WEB-INF/completed-matches.jsp")
                 .forward(request, response);
