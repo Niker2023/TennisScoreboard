41591c361310dc9fcf0b73d3426cde48d6fb947e
Niker2023 <berserg@bk.ru>
2025-03-24 23:23:53 +0300

    feat: add methods to implement pagination

--- src/main/java/com/project/dao/MatchDao.java
+++ src/main/java/com/project/dao/MatchDao.java
@@ -50,4 +50,26 @@ public class MatchDao {
         session.getTransaction().commit();
         return matchesList;
     }
+
+
+    public List<Matches> getMatchesForPagination(Integer page, Integer limit) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query<Matches> query = session.createQuery("from Matches", Matches.class);
+        query.setFirstResult((page - 1) * limit);
+        query.setMaxResults(limit);
+        List<Matches> matchesList = query.getResultList();
+        session.getTransaction().commit();
+        return matchesList;
+    }
+
+
+    public Long getMatchesCount() {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query<Long> query = session.createQuery("select count(*) from Matches", Long.class);
+        Long count = query.getSingleResult();
+        session.getTransaction().commit();
+        return count;
+    }
 }
