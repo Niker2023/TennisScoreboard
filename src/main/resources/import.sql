1a2f2240ba2905f59fe32207fb1ba347c129dd4b
Niker2023 <berserg@bk.ru>
2025-03-24 23:25:01 +0300

    refactor: add more matches for tests

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,5 +8,21 @@ INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (2,3,2);
 INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (3,2,2);
 INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
 INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+
 INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
 INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (3,4,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
+
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (3,4,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
+
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (3,4,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (1,3,3);
+INSERT INTO Matches(player1_id, player2_id, winner_id) VALUES (4,1,4);
