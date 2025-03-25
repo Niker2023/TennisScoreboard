966d0a2ce21e352b985a23320862839eb0158498
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-25 11:52:50 +0300

    feat: add methods to get matches by player and count matches by player for pagination

--- src/main/java/com/project/dao/MatchDao.java
+++ src/main/java/com/project/dao/MatchDao.java
@@ -32,32 +32,24 @@ public class MatchDao {
     }
 
 
-    public List<Matches> getMatches() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query<Matches> query = session.createQuery("from Matches", Matches.class);
-        List<Matches> matches = query.getResultList();
-        session.getTransaction().commit();
-        return matches;
-    }
-
-
-    public List<Matches> getMatchesByPlayer(Players player) {
+    public List<Matches> getMatchesByPlayer(Players player, Integer currentPage, Integer limitPerPage) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
         Query<Matches> query = session.createQuery("from Matches where player1 = :player or player2 = :player", Matches.class);
+        query.setFirstResult((currentPage - 1) * limitPerPage);
+        query.setMaxResults(limitPerPage);
         List<Matches> matchesList = query.setParameter("player", player).getResultList();
         session.getTransaction().commit();
         return matchesList;
     }
 
 
-    public List<Matches> getMatchesForPagination(Integer page, Integer limit) {
+    public List<Matches> getMatches(Integer currentPage, Integer limitPerPage) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
         Query<Matches> query = session.createQuery("from Matches", Matches.class);
-        query.setFirstResult((page - 1) * limit);
-        query.setMaxResults(limit);
+        query.setFirstResult((currentPage - 1) * limitPerPage);
+        query.setMaxResults(limitPerPage);
         List<Matches> matchesList = query.getResultList();
         session.getTransaction().commit();
         return matchesList;
@@ -69,6 +61,16 @@ public class MatchDao {
         session.beginTransaction();
         Query<Long> query = session.createQuery("select count(*) from Matches", Long.class);
         Long count = query.getSingleResult();
+        session.getTransaction().commit();
+        return count;
+    }
+
+
+    public Long getMatchesCountByPlayer(Players player) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Query<Long> query = session.createQuery("select count(*) from Matches where player1 = :player or player2 = :player", Long.class);
+        Long count = query.setParameter("player", player).getSingleResult();
         session.getTransaction().commit();
         return count;
     }
