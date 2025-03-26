ba5c9594d2c926986e1a5407e42899ee1a214ea3
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-26 16:57:52 +0300

    refactor: delete the getNameById method

--- src/main/java/com/project/dao/PlayerDao.java
+++ src/main/java/com/project/dao/PlayerDao.java
@@ -27,15 +27,6 @@ public class PlayerDao {
     }
 
 
-    public String getNameById(int id) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Players player = session.get(Players.class, id);
-        session.getTransaction().commit();
-        return player.getName();
-    }
-
-
     public Players getPlayerById(int id) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         session.beginTransaction();
