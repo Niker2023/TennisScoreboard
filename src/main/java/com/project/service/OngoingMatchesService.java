0c0e468636fb79b0ddc31e11847672c1b8898b4b
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-27 16:34:30 +0300

    feat: delete static methods

--- src/main/java/com/project/service/OngoingMatchesService.java
+++ src/main/java/com/project/service/OngoingMatchesService.java
@@ -6,19 +6,19 @@ import java.util.*;
 
 public class OngoingMatchesService {
 
-    private static final Map<UUID, MatchScore> matches = new HashMap<>();
+    private final Map<UUID, MatchScore> matches = new HashMap<>();
 
-    public static UUID addMatch(MatchScore matchScore) {
+    public UUID addMatch(MatchScore matchScore) {
         UUID uuid = UUID.randomUUID();
         matches.put(uuid, matchScore);
         return uuid;
     }
 
-    public static MatchScore getMatch(UUID uuid) {
+    public MatchScore getMatch(UUID uuid) {
         return matches.get(uuid);
     }
 
-    public static void removeMatch(UUID uuid) {
+    public void removeMatch(UUID uuid) {
         matches.remove(uuid);
     }
 }
