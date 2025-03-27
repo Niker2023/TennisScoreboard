ca8a17f3988e34a814ee0e8069b0c8621b8b3f06
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-27 14:50:02 +0300

    feat: add a transaction rollback mechanism

--- src/main/java/com/project/dao/PlayerDao.java
+++ src/main/java/com/project/dao/PlayerDao.java
@@ -1,11 +1,13 @@
 package com.project.dao;
 
 import com.project.entity.Players;
+import com.project.exception.HibernateException;
 import com.project.util.HibernateUtil;
 import jakarta.persistence.PersistenceException;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 @Slf4j
@@ -13,29 +15,53 @@ import org.hibernate.query.Query;
 public class PlayerDao {
 
     public void save(Players player) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.persist(player);
-        log.info("save player : {}", player);
-        session.getTransaction().commit();
+        Transaction transaction = null;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            transaction = session.beginTransaction();
+            session.persist(player);
+            log.info("save player : {}", player);
+            transaction.commit();
+        } catch (PersistenceException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new HibernateException("Can`t save player : " + player);
+        }
     }
 
-
     public Players getPlayerById(int id) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Players player = session.get(Players.class, id);
-        session.getTransaction().commit();
+        Transaction transaction = null;
+        Players player;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            transaction = session.beginTransaction();
+            player = session.get(Players.class, id);
+            transaction.commit();
+        } catch (PersistenceException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new HibernateException("Can`t get player : " + id);
+        }
         return player;
     }
 
-
-    public Players getPlayerByName(String name) throws PersistenceException {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query<Players> query = session.createQuery("from Players where name = :name", Players.class);
-        Players player = query.setParameter("name", name).getSingleResult();
-        session.getTransaction().commit();
+    public Players getPlayerByName(String name) {
+        Transaction transaction = null;
+        Players player;
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            transaction = session.beginTransaction();
+            Query<Players> query = session.createQuery("from Players where name = :name", Players.class);
+            player = query.setParameter("name", name).getSingleResult();
+            transaction.commit();
+        } catch (PersistenceException e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new HibernateException("Can`t get player : " + name);
+        }
         return player;
     }
 }
