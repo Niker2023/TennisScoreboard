d117205eed726afc58f0985dcbb8ff8ce06f0c8d
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-27 14:35:50 +0300

    style: delete the empty lines

--- src/main/java/com/project/servlet/HomePageServlet.java
+++ src/main/java/com/project/servlet/HomePageServlet.java
@@ -1,7 +1,6 @@
 package com.project.servlet;
 
 import java.io.*;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -10,10 +9,8 @@ import jakarta.servlet.annotation.*;
 public class HomePageServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
         request.setAttribute("new_match_url", "new-match");
         request.setAttribute("matches_url", "matches");
-
         request.getRequestDispatcher("/WEB-INF/home-page.jsp")
                 .forward(request, response);
     }
