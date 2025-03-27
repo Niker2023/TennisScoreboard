3c01ef4c402c50164cdd6d16116c7c79412776aa
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-27 15:00:59 +0300

    feat: change the MatchScore collection structure: create the playerName variables and isMatchOver variable

--- src/main/java/com/project/entity/MatchScore.java
+++ src/main/java/com/project/entity/MatchScore.java
@@ -2,131 +2,129 @@ package com.project.entity;
 
 import com.project.dto.PlayerDto;
 import com.project.util.Player;
+import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MatchScore {
 
-    private final Map<String, String> scores;
+    private final Map<String, Integer> scores;
+    @Getter
+    private final String player1Name;
+    @Getter
+    private final String player2Name;
+    @Getter
+    private boolean isMatchOver = false;
 
     public MatchScore(PlayerDto player1, PlayerDto player2) {
+        player1Name = player1.playerName();
+        player2Name = player2.playerName();
         scores = new HashMap<>();
-        scores.put("player1Id", player1.id().toString());
-        scores.put("player2Id", player2.id().toString());
-        scores.put("player1Name", player1.playerName());
-        scores.put("player2Name", player2.playerName());
-        scores.put("player1Points", "0");
-        scores.put("player2Points", "0");
-        scores.put("player1Set1Score", "0");
-        scores.put("player2Set1Score", "0");
-        scores.put("player1Set2Score", "0");
-        scores.put("player2Set2Score", "0");
-        scores.put("player1Set3Score", "0");
-        scores.put("player2Set3Score", "0");
-        scores.put("winnerId", "0");
+        scores.put("player1Id", player1.id());
+        scores.put("player2Id", player2.id());
+        scores.put("player1Points", 0);
+        scores.put("player2Points", 0);
+        scores.put("player1Set1Score", 0);
+        scores.put("player2Set1Score", 0);
+        scores.put("player1Set2Score", 0);
+        scores.put("player2Set2Score", 0);
+        scores.put("player1Set3Score", 0);
+        scores.put("player2Set3Score", 0);
+        scores.put("winnerId", 0);
     }
 
 
     public Integer getPoints(String player) {
         if (player.equals(Player.ONE.toString())) {
-            return Integer.parseInt(scores.get("player1Points"));
+            return scores.get("player1Points");
         }
-        return Integer.parseInt(scores.get("player2Points"));
+        return scores.get("player2Points");
     }
 
 
     public Integer getSet1Score(String player) {
         if (player.equals(Player.ONE.toString())) {
-            return Integer.parseInt(scores.get("player1Set1Score"));
+            return scores.get("player1Set1Score");
         }
-        return Integer.parseInt(scores.get("player2Set1Score"));
+        return scores.get("player2Set1Score");
     }
 
 
     public Integer getSet2Score(String player) {
         if (player.equals(Player.ONE.toString())) {
-            return Integer.parseInt(scores.get("player1Set2Score"));
+            return scores.get("player1Set2Score");
         }
-        return Integer.parseInt(scores.get("player2Set2Score"));
+        return scores.get("player2Set2Score");
     }
 
 
     public Integer getSet3Score(String player) {
         if (player.equals(Player.ONE.toString())) {
-            return Integer.parseInt(scores.get("player1Set3Score"));
+            return scores.get("player1Set3Score");
         }
-        return Integer.parseInt(scores.get("player2Set3Score"));
+        return scores.get("player2Set3Score");
     }
 
 
     public Integer getPlayer1Id() {
-        return Integer.parseInt(scores.get("player1Id"));
+        return scores.get("player1Id");
     }
 
 
     public Integer getPlayer2Id() {
-        return Integer.parseInt(scores.get("player2Id"));
+        return scores.get("player2Id");
     }
 
 
     public Integer getWinnerId() {
-        return Integer.parseInt(scores.get("winnerId"));
+        return scores.get("winnerId");
     }
 
 
     public void setPoints(String player, Integer points) {
         if (player.equals(Player.ONE.toString())) {
-            scores.put("player1Points", points.toString());
+            scores.put("player1Points", points);
         } else {
-            scores.put("player2Points", points.toString());
+            scores.put("player2Points", points);
         }
     }
 
 
     public void setSet1Score(String player, Integer score) {
         if (player.equals(Player.ONE.toString())) {
-            scores.put("player1Set1Score", score.toString());
+            scores.put("player1Set1Score", score);
         } else {
-            scores.put("player2Set1Score", score.toString());
+            scores.put("player2Set1Score", score);
         }
     }
 
 
     public void setSet2Score(String player, Integer score) {
         if (player.equals(Player.ONE.toString())) {
-            scores.put("player1Set2Score", score.toString());
+            scores.put("player1Set2Score", score);
         } else {
-            scores.put("player2Set2Score", score.toString());
+            scores.put("player2Set2Score", score);
         }
     }
 
 
     public void setSet3Score(String player, Integer score) {
         if (player.equals(Player.ONE.toString())) {
-            scores.put("player1Set3Score", score.toString());
+            scores.put("player1Set3Score", score);
         } else {
-            scores.put("player2Set3Score", score.toString());
+            scores.put("player2Set3Score", score);
         }
     }
 
 
     public void setTheWinnerOfTheMatch(Integer winnerId) {
-        scores.put("winnerId", winnerId.toString());
+        scores.put("winnerId", winnerId);
+        isMatchOver = true;
     }
 
 
-    public String getPlayer1Name() {
-        return scores.get("player1Name");
-    }
-
-
-    public String getPlayer2Name() {
-        return scores.get("player2Name");
-    }
-
-
-    private Map<String, String> getMap() {
+    private Map<String, Integer> getMap() {
         return scores;
     }
 
