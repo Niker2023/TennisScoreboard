926315cde7dd3259ae311247c64ddbbc1a3dd3af
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-27 14:50:42 +0300

    style: delete the empty lines

--- src/main/java/com/project/servlet/NewMatchServlet.java
+++ src/main/java/com/project/servlet/NewMatchServlet.java
@@ -19,18 +19,14 @@ import java.util.regex.Pattern;
 public class NewMatchServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         request.getRequestDispatcher("/WEB-INF/new-match.jsp")
                 .forward(request, response);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         var player1 = request.getParameter("player1");
         var player2 = request.getParameter("player2");
-
         Pattern patternName = Pattern.compile("^[A-Za-zА-Яа-яЁё\\s-.]+$");
-
         if (player1 == null || player2 == null || player1.isBlank() || player2.isBlank()
                 || !patternName.matcher(player1).matches() || !patternName.matcher(player2).matches()) {
             request.setAttribute("error", true);
@@ -42,7 +38,6 @@ public class NewMatchServlet extends HttpServlet {
                     .forward(request, response);
             return;
         }
-
         if (player1.equals(player2)) {
             request.setAttribute("error", true);
             request.setAttribute("error_message", "Имена игроков одинаковы!");
@@ -51,7 +46,6 @@ public class NewMatchServlet extends HttpServlet {
                     .forward(request, response);
             return;
         }
-
         var playersPersistenceService = new PlayersPersistenceService();
         PlayerDto player1Dto;
         PlayerDto player2Dto;
@@ -59,27 +53,22 @@ public class NewMatchServlet extends HttpServlet {
             player1Dto = playersPersistenceService.save(player1);
         } catch (PersistenceException e) {
             request.setAttribute("error", true);
-            request.setAttribute("error_message", "Имя первого игрока уже существует в базе данных! Введите другое.");
-
+            request.setAttribute("error_message", "Имя первого игрока уже существует! Введите другое.");
             request.getRequestDispatcher("/WEB-INF/new-match.jsp")
                     .forward(request, response);
             return;
         }
-
         try {
             player2Dto = playersPersistenceService.save(player2);
         } catch (PersistenceException e) {
             request.setAttribute("error", true);
-            request.setAttribute("error_message", "Имя второго игрока уже существует в базе данных! Введите другое.");
-
+            request.setAttribute("error_message", "Имя второго игрока уже существует! Введите другое.");
             request.getRequestDispatcher("/WEB-INF/new-match.jsp")
                     .forward(request, response);
             return;
         }
-
         MatchScore newMatch = new MatchScore(player1Dto, player2Dto);
         UUID newMatchUuid = OngoingMatchesService.addMatch(newMatch);
-
         response.sendRedirect(request.getContextPath() + "/match-score?uuid=" + newMatchUuid);
     }
 }
