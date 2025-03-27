25d0e4646b586968b177e63c59229ae853ddf9c8
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-27 14:51:36 +0300

    feat: add service call try-catch and error handling

--- src/main/java/com/project/servlet/MatchScoreServlet.java
+++ src/main/java/com/project/servlet/MatchScoreServlet.java
@@ -1,5 +1,6 @@
 package com.project.servlet;
 
+import com.project.exception.HibernateException;
 import com.project.service.FinishedMatchesPersistenceService;
 import com.project.service.MatchScoreCalculationService;
 import com.project.service.OngoingMatchesService;
@@ -9,47 +10,40 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.util.UUID;
 
+@Slf4j
 @WebServlet("/match-score")
 public class MatchScoreServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         UUID currentUuid = UUID.fromString(request.getParameter("uuid"));
-
         var currentMatch = OngoingMatchesService.getMatch(currentUuid);
-
         HttpServletRequest filledRequest = MatchScoreViewUtil.getFilledRequest(request, currentMatch);
-
         filledRequest.setAttribute("uuid", currentUuid);
-
         request.getRequestDispatcher("/WEB-INF/match-score.jsp")
                 .forward(filledRequest, response);
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
         UUID currentUuid = UUID.fromString(request.getParameter("uuid"));
         String winner = request.getParameter("winner");
-
         var currentMatch = OngoingMatchesService.getMatch(currentUuid);
-
         MatchScoreCalculationService.changeMatchScore(currentMatch, winner);
-
         HttpServletRequest filledRequest = MatchScoreViewUtil.getFilledRequest(request, currentMatch);
-
         filledRequest.setAttribute("uuid", currentUuid);
-
         request.getRequestDispatcher("/WEB-INF/match-score.jsp")
                 .forward(filledRequest, response);
-
-        if (!currentMatch.getWinnerId().equals(0)) {
-
-            var finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
-            finishedMatchesPersistenceService.save(currentMatch);
+        if (currentMatch.isMatchOver()) {
+            try {
+                var finishedMatchesPersistenceService = new FinishedMatchesPersistenceService();
+                finishedMatchesPersistenceService.save(currentMatch);
+            } catch (HibernateException e) {
+                log.error(e.getMessage(), e);
+            }
             OngoingMatchesService.removeMatch(currentUuid);
         }
     }
