446904c7e65bdf80a601bb2fb2de5b106dc22e46
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-28 17:12:44 +0300

    refactor: transfer the business logic to the service and redesign error handling

--- src/main/java/com/project/servlet/NewMatchServlet.java
+++ src/main/java/com/project/servlet/NewMatchServlet.java
@@ -1,10 +1,7 @@
 package com.project.servlet;
 
-import com.project.dto.PlayerDto;
-import com.project.entity.MatchScore;
-import com.project.service.OngoingMatchesService;
-import com.project.service.PlayersPersistenceService;
-import jakarta.persistence.PersistenceException;
+import com.project.exception.ValidationException;
+import com.project.service.MatchCreationService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -13,10 +10,16 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.UUID;
-import java.util.regex.Pattern;
 
 @WebServlet("/new-match")
 public class NewMatchServlet extends HttpServlet {
+
+    private MatchCreationService matchCreationService;
+
+    @Override
+    public void init() {
+        matchCreationService = new MatchCreationService();
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/new-match.jsp")
@@ -24,52 +27,24 @@ public class NewMatchServlet extends HttpServlet {
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        var player1 = request.getParameter("player1");
-        var player2 = request.getParameter("player2");
-        Pattern patternName = Pattern.compile("^[A-Za-zА-Яа-яЁё\\s-.]+$");
-        if (player1 == null || player2 == null || player1.isBlank() || player2.isBlank()
-                || !patternName.matcher(player1).matches() || !patternName.matcher(player2).matches()) {
-            request.setAttribute("error", true);
-            request.setAttribute("error_message", """
-                    Имя введено некорректно! <br>
-                                    Допустимы для ввода: буквы, пробел, точка, дефис.<br>
-                                    Максимальное длина имени 20 символов.""");
-            request.getRequestDispatcher("/WEB-INF/new-match.jsp")
-                    .forward(request, response);
-            return;
-        }
-        if (player1.equals(player2)) {
-            request.setAttribute("error", true);
-            request.setAttribute("error_message", "Имена игроков одинаковы!");
 
-            request.getRequestDispatcher("/WEB-INF/new-match.jsp")
-                    .forward(request, response);
-            return;
-        }
-        var playersPersistenceService = new PlayersPersistenceService();
-        PlayerDto player1Dto;
-        PlayerDto player2Dto;
         try {
-            player1Dto = playersPersistenceService.save(player1);
-        } catch (PersistenceException e) {
+            var player1 = request.getParameter("player1");
+            var player2 = request.getParameter("player2");
+
+            if (player1 == null || player2 == null || player1.isBlank() || player2.isBlank()) {
+                throw new ValidationException("Имя игрока не может быть пустым.");
+            }
+
+            UUID newMatchUUID = matchCreationService.createMatch(player1, player2);
+
+            response.sendRedirect(request.getContextPath() + "/match-score?uuid=" + newMatchUUID);
+
+        } catch (Exception e) {
             request.setAttribute("error", true);
-            request.setAttribute("error_message", "Имя первого игрока уже существует! Введите другое.");
+            request.setAttribute("error_message", e.getMessage());
             request.getRequestDispatcher("/WEB-INF/new-match.jsp")
                     .forward(request, response);
-            return;
         }
-        try {
-            player2Dto = playersPersistenceService.save(player2);
-        } catch (PersistenceException e) {
-            request.setAttribute("error", true);
-            request.setAttribute("error_message", "Имя второго игрока уже существует! Введите другое.");
-            request.getRequestDispatcher("/WEB-INF/new-match.jsp")
-                    .forward(request, response);
-            return;
-        }
-        MatchScore newMatch = new MatchScore(player1Dto, player2Dto);
-        OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
-        UUID newMatchUuid = ongoingMatchesService.addMatch(newMatch);
-        response.sendRedirect(request.getContextPath() + "/match-score?uuid=" + newMatchUuid);
     }
 }
