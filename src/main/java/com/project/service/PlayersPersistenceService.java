94669406e9720610637b2bdf5ddf6000d3ddd0c2
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-28 16:49:34 +0300

    style: add empty lines

--- src/main/java/com/project/service/PlayersPersistenceService.java
+++ src/main/java/com/project/service/PlayersPersistenceService.java
@@ -11,7 +11,9 @@ public class PlayersPersistenceService {
     public PlayerDto save(String player) {
         PlayerDao playerDao = new PlayerDao();
         Players newPlayer = new Players(player);
+
         playerDao.save(newPlayer);
+
         return new PlayerDto(newPlayer.getId(), newPlayer.getName());
     }
 }
