64238797bcbdee2e9e5f76cc416c234b16b62201
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-31 17:51:14 +0300

    feat: change the source of the variable

--- src/main/java/com/project/entity/matchState/GameState.java
+++ src/main/java/com/project/entity/matchState/GameState.java
@@ -9,13 +9,13 @@ public class GameState extends PointsState {
     @Override
     public void changePoints() {
 
-        matchState.score.setPoints(winner, matchState.score.getPoints(winner) + 1);
+        matchState.score.setPoints(matchState.winner, matchState.score.getPoints(matchState.winner) + 1);
 
-        if (matchState.getScore().getPoints(winner) > 3
-                && (matchState.score.getPoints(winner) - matchState.score.getPoints(matchState.loser)) > 1) {
-            matchState.score.setSet1Score(winner, matchState.score.getSet1Score(winner) + 1);
+        if (matchState.getScore().getPoints(matchState.winner) > 3
+                && (matchState.score.getPoints(matchState.winner) - matchState.score.getPoints(matchState.loser)) > 1) {
+            matchState.score.setSet1Score(matchState.winner, matchState.score.getSet1Score(matchState.winner) + 1);
 
-            if (matchState.getScore().getSetScore(winner, matchState.toString()) == 6
+            if (matchState.getScore().getSetScore(matchState.winner, matchState.toString()) == 6
                     && matchState.getScore().getSetScore(matchState.loser, matchState.toString()) == 6) {
                 matchState.pointsState = new TiebreakState(matchState);
             } else {
