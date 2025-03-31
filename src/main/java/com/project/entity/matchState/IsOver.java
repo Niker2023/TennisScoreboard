57830389e8e9a016d75088bed615f5e195459f0c
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-03-31 17:47:47 +0300

    refactor: remove useless classes

--- src/main/java/com/project/entity/matchState/IsOver.java
+++
@@ -1,9 +0,0 @@
-package com.project.entity.matchState;
-
-import com.project.entity.NewMatchScore;
-
-public class IsOver extends MatchState {
-    IsOver(NewMatchScore newMatchScore) {
-        super(newMatchScore);
-    }
-}
