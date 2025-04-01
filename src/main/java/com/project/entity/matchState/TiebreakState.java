61e48147ce4a2b584d1f26b104ce459a798f1910
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-01 17:47:34 +0300

    feat: add business logic

--- src/main/java/com/project/entity/matchState/TiebreakState.java
+++ src/main/java/com/project/entity/matchState/TiebreakState.java
@@ -8,5 +8,18 @@ public class TiebreakState extends PointsState {
     @Override
     public void changePoints() {
 
+        increasePoints();
+
+        if (isTiebreakOver()) {
+            matchState.increaseSetScore();
+
+            matchState.pointsState = new GameState(matchState);
+        }
+    }
+
+
+    private boolean isTiebreakOver() {
+        return matchState.getScore().getPoints(matchState.winner) > 5
+                && (matchState.score.getPoints(matchState.winner) - matchState.score.getPoints(matchState.loser)) > 1;
     }
 }
