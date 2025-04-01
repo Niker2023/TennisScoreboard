3654fe48da1cd773c3a787f74e2a306107d02fde
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-01 17:45:39 +0300

    feat: add some new methods

--- src/main/java/com/project/entity/matchState/MatchState.java
+++ src/main/java/com/project/entity/matchState/MatchState.java
@@ -1,5 +1,6 @@
 package com.project.entity.matchState;
 
+import com.project.util.Set;
 import lombok.Getter;
 
 public abstract class MatchState {
@@ -19,4 +20,26 @@ public abstract class MatchState {
 
 
     public abstract void changeScore();
+
+
+    void increaseSetScore() {
+        switch (this.getClass().getSimpleName()) {
+            case "SetOneState" -> {
+                score.setSet1Score(winner, score.getSet1Score(winner) + 1);
+            }
+            case "SetTwoState" -> {
+                score.setSet2Score(winner, score.getSet2Score(winner) + 1);
+            }
+            case "SetThreeState" -> {
+                score.setSet3Score(winner, score.getSet3Score(winner) + 1);
+            }
+        }
+    }
+
+
+    boolean isSetOver() {
+        return ((score.getSetScore(winner, this.getClass().getSimpleName()) > 5
+                && (score.getSetScore(winner, this.getClass().getSimpleName()) - score.getSetScore(loser, this.getClass().getSimpleName()) > 1)))
+                || score.getSetScore(winner, this.getClass().getSimpleName()) == 7;
+    }
 }
