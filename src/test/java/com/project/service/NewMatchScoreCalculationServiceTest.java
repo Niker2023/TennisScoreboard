66bd7ddd2c91418ba91d9d6313179c016b700c84
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-01 17:44:34 +0300

    feat: add a new test class for a new score class

---
+++ src/test/java/com/project/service/NewMatchScoreCalculationServiceTest.java
@@ -0,0 +1,266 @@
+package com.project.service;
+
+import com.project.dto.PlayerDto;
+import com.project.entity.matchState.Score;
+import com.project.util.Player;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NewMatchScoreCalculationServiceTest {
+
+    @Test
+    void whenPlayer1WinsPoint_thenSuccess() {
+        Score testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints(Player.ONE.toString(),3);
+        testMatch.setPoints(Player.TWO.toString(),3);
+
+        testMatch.playerWinsPointByPlayerOrder(Player.ONE.toString());
+
+        Score expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints(Player.ONE.toString(),4);
+        expectedMatchResult.setPoints(Player.TWO.toString(),3);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer2WinsPointAndGameWithFirstAD_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",4);
+
+        testMatch.playerWinsPointByPlayerOrder(Player.TWO.toString());
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player2", 1);
+
+         assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer1WinsPointAndGameWith_40_0_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",0);
+
+        testMatch.playerWinsPointByPlayerOrder(Player.ONE.toString());
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setSet1Score("player1", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer2WinsPointAndGameWithMoreThen4PointsAndAD_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints("player1",5);
+        testMatch.setPoints("player2",6);
+        testMatch.playerWinsPointByPlayerOrder(Player.TWO.toString());
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player2", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer1WinsPointAndGameAndSet1_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",2);
+        testMatch.setSet1Score("player1", 5);
+        testMatch.setSet1Score("player2", 4);
+
+        testMatch.playerWinsPointByPlayerOrder(Player.ONE.toString());
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 6);
+        expectedMatchResult.setSet1Score("player2", 4);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer2WinsPointAndGameAndSet1AndGameInSet2_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints("player2",3);
+        testMatch.setPoints("player1",2);
+        testMatch.setSet1Score("player2", 5);
+        testMatch.setSet1Score("player1", 4);
+
+        for (int i = 0; i < 5; i++) {
+            testMatch.playerWinsPointByPlayerOrder(Player.TWO.toString());
+        }
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 4);
+        expectedMatchResult.setSet1Score("player2", 6);
+        expectedMatchResult.setSet2Score("player2", 1);
+        expectedMatchResult.setSet2Score("player1", 0);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenScoreInSet1_5_6_AndPlayer1WinsGame_thenTiebreak_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.setPoints("player1",0);
+        testMatch.setPoints("player2",0);
+        testMatch.setSet1Score("player1", 5);
+        testMatch.setSet1Score("player2", 6);
+        for (int i = 0; i < 9; i++) {
+            testMatch.playerWinsPointByPlayerOrder(Player.ONE.toString());
+        }
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",5);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 6);
+        expectedMatchResult.setSet1Score("player2", 6);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenTiebreakAndPlayer1Wins10ScoresInSet2_thenStartSet3_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.SetStatesForTests(2,true);
+
+        testMatch.setPoints("player1",0);
+        testMatch.setPoints("player2",0);
+        testMatch.setSet1Score("player1", 5);
+        testMatch.setSet1Score("player2", 7);
+        testMatch.setSet2Score("player1", 6);
+        testMatch.setSet2Score("player2", 6);
+        for (int i = 0; i < 10; i++) {
+            testMatch.playerWinsPointByPlayerOrder(Player.ONE.toString());
+        }
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",0);
+        expectedMatchResult.setSet1Score("player1", 5);
+        expectedMatchResult.setSet1Score("player2", 7);
+        expectedMatchResult.setSet2Score("player1", 7);
+        expectedMatchResult.setSet2Score("player2", 6);
+        expectedMatchResult.setSet3Score("player1", 1);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenTiebreakAndPlayer2Wins2PointsWhen_7_7_thenPlayer2WinsTiebreak_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.SetStatesForTests(2, true);
+
+        testMatch.setPoints("player1",7);
+        testMatch.setPoints("player2",7);
+        testMatch.setSet1Score("player1", 7);
+        testMatch.setSet1Score("player2", 5);
+        testMatch.setSet2Score("player1", 6);
+        testMatch.setSet2Score("player2", 6);
+        for (int i = 0; i < 4; i++) {
+            testMatch.playerWinsPointByPlayerOrder(Player.TWO.toString());
+        }
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setPoints("player1",0);
+        expectedMatchResult.setPoints("player2",2);
+        expectedMatchResult.setSet1Score("player1", 7);
+        expectedMatchResult.setSet1Score("player2", 5);
+        expectedMatchResult.setSet2Score("player1", 6);
+        expectedMatchResult.setSet2Score("player2", 7);
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer1WinsGameAndMatch_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.SetStatesForTests(2, false);
+
+        testMatch.setPoints("player1",3);
+        testMatch.setPoints("player2",2);
+        testMatch.setSet1Score("player1", 7);
+        testMatch.setSet1Score("player2", 6);
+        testMatch.setSet2Score("player1", 5);
+        testMatch.setSet2Score("player2", 4);
+
+        testMatch.playerWinsPointByPlayerOrder(Player.ONE.toString());
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+        expectedMatchResult.setSet1Score("player1", 7);
+        expectedMatchResult.setSet1Score("player2", 6);
+        expectedMatchResult.setSet2Score("player1", 6);
+        expectedMatchResult.setSet2Score("player2", 4);
+
+        expectedMatchResult.setWinnerId(Player.ONE.toString());
+        expectedMatchResult.setMatchIsOver();
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+
+    @Test
+    void whenPlayer2WinsTiebreakInSet3_thenSuccess() {
+        var testMatch = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        testMatch.SetStatesForTests(3, true);
+
+        testMatch.setPoints("player1",7);
+        testMatch.setPoints("player2",7);
+        testMatch.setSet1Score("player1", 7);
+        testMatch.setSet1Score("player2", 5);
+        testMatch.setSet2Score("player1", 6);
+        testMatch.setSet2Score("player2", 7);
+        testMatch.setSet3Score("player1", 6);
+        testMatch.setSet3Score("player2", 6);
+
+        for (int i = 0; i < 2; i++) {
+            testMatch.playerWinsPointByPlayerOrder(Player.TWO.toString());
+        }
+
+        var expectedMatchResult = new Score(new PlayerDto(1,""), new PlayerDto(2,""));
+
+        expectedMatchResult.setSet1Score("player1", 7);
+        expectedMatchResult.setSet1Score("player2", 5);
+        expectedMatchResult.setSet2Score("player1", 6);
+        expectedMatchResult.setSet2Score("player2", 7);
+        expectedMatchResult.setSet3Score("player1", 6);
+        expectedMatchResult.setSet3Score("player2", 7);
+
+        expectedMatchResult.setWinnerId(Player.TWO.toString());
+        expectedMatchResult.setMatchIsOver();
+
+        assertEquals(testMatch, expectedMatchResult);
+    }
+}
