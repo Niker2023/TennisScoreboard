15bdbe752b53165b1bc17ba54cae95ee18e1a433
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-01 17:47:30 +0300

    feat: add business logic

--- src/main/java/com/project/entity/matchState/SetTwoState.java
+++ src/main/java/com/project/entity/matchState/SetTwoState.java
@@ -9,5 +9,21 @@ public class SetTwoState extends MatchState {
     @Override
     public void changeScore() {
 
+        pointsState.changePoints();
+
+        if (isSetOver()) {
+            if (isMatchOver()) {
+                score.setMatchOver(true);
+                score.setWinnerId(winner);
+            } else {
+                score.setMatchState(new SetThreeState(score));
+            }
+        }
+    }
+
+
+    private boolean isMatchOver() {
+        return score.getSet1Score(winner) == 7
+                || (score.getSet1Score(winner) == 6 && (score.getSet1Score(winner) - score.getSet1Score(loser) > 1));
     }
 }
