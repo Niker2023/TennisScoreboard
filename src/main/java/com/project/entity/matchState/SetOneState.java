cd33cc164bd1549beaf3e50e3db2d7f12ecef62e
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-01 17:46:25 +0300

    feat: add code simplification and bug fixes

--- src/main/java/com/project/entity/matchState/SetOneState.java
+++ src/main/java/com/project/entity/matchState/SetOneState.java
@@ -7,30 +7,12 @@ public class SetOneState extends MatchState {
     }
 
     @Override
-    public String toString() {
-        return "set1";
-    }
-
-    @Override
     public void changeScore() {
-        score.setPoints(winner, score.getPoints(winner) + 1);
 
-        if (score.getPoints(winner) > 3
-                && (score.getPoints(winner) - score.getPoints(loser)) > 1) {
+        pointsState.changePoints();
 
-            score.setSet1Score(winner, score.getSet1Score(winner) + 1);
-
-            if (score.getSetScore(winner, score.getMatchState().toString()) == 6
-                    && score.getSetScore(loser, score.getMatchState().toString()) == 6) {
-                if (score.getPoints(winner) > 6
-                        && (score.getPoints(winner) - score.getPoints(loser)) > 1) {
-                    score.setMatchState(new SetTwoState(score));
-                }
-            } else if (score.getSetScore(winner, score.getMatchState().toString()) > 5
-                    && (score.getSetScore(winner, score.getMatchState().toString())
-                    - score.getSetScore(loser, score.getMatchState().toString()) ) > 1) {
-                score.setMatchState(new SetTwoState(score));
-            }
+        if (isSetOver()) {
+            score.setMatchState(new SetTwoState(score));
         }
     }
 }
