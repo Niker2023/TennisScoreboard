ed947676315281dac89e8fa9b7423faab26c64e4
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-01 17:39:37 +0300

    feat: add code simplification and bug fixes

--- src/main/java/com/project/entity/matchState/GameState.java
+++ src/main/java/com/project/entity/matchState/GameState.java
@@ -9,18 +9,26 @@ public class GameState extends PointsState {
     @Override
     public void changePoints() {
 
-        matchState.score.setPoints(matchState.winner, matchState.score.getPoints(matchState.winner) + 1);
+        increasePoints();
 
-        if (matchState.getScore().getPoints(matchState.winner) > 3
-                && (matchState.score.getPoints(matchState.winner) - matchState.score.getPoints(matchState.loser)) > 1) {
-            matchState.score.setSet1Score(matchState.winner, matchState.score.getSet1Score(matchState.winner) + 1);
+        if (isGameOver()) {
+            matchState.increaseSetScore();
 
-            if (matchState.getScore().getSetScore(matchState.winner, matchState.toString()) == 6
-                    && matchState.getScore().getSetScore(matchState.loser, matchState.toString()) == 6) {
-                matchState.pointsState = new TiebreakState(matchState);
-            } else {
-                matchState.pointsState = new GameState(matchState);
-            }
+            matchState.pointsState = isNextTiebreak()
+                    ? new TiebreakState(matchState)
+                    : new GameState(matchState);
         }
     }
+
+
+     private boolean isNextTiebreak() {
+        return matchState.getScore().getSetScore(matchState.winner, matchState.getClass().getSimpleName()) == 6
+                && matchState.getScore().getSetScore(matchState.loser, matchState.getClass().getSimpleName()) == 6;
+     }
+
+
+     private boolean isGameOver() {
+        return matchState.getScore().getPoints(matchState.winner) > 3
+                && (matchState.score.getPoints(matchState.winner) - matchState.score.getPoints(matchState.loser)) > 1;
+     }
 }
