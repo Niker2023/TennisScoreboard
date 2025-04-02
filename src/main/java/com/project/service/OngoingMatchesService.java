5f39a717ddfe29847279b31e5e38a75938afcc1d
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-02 17:45:46 +0300

    feat: change Score class

--- src/main/java/com/project/service/OngoingMatchesService.java
+++ src/main/java/com/project/service/OngoingMatchesService.java
@@ -1,21 +1,22 @@
 package com.project.service;
 
 import com.project.entity.MatchScore;
+import com.project.entity.matchState.Score;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class OngoingMatchesService {
 
-    private static final Map<UUID, MatchScore> matches = new ConcurrentHashMap<>();
+    private static final Map<UUID, Score> matches = new ConcurrentHashMap<>();
 
-    public UUID addMatch(MatchScore matchScore) {
+    public UUID addMatch(Score matchScore) {
         UUID uuid = UUID.randomUUID();
         matches.put(uuid, matchScore);
         return uuid;
     }
 
-    public MatchScore getMatch(UUID uuid) {
+    public Score getMatch(UUID uuid) {
         return matches.get(uuid);
     }
 
