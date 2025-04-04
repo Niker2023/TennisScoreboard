dd52c181b88040fab3b55dcc82c4a650cf36ed84
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-04 15:22:42 +0300

    feat: add service call try-catch and error handling

--- src/main/java/com/project/dao/MatchDao.java
+++ src/main/java/com/project/dao/MatchDao.java
@@ -1,12 +1,17 @@
 package com.project.dao;
 
-import com.project.entity.Matches;
 import com.project.entity.Players;
+import com.project.entity.Matches;
+import com.project.exception.DaoException;
 import com.project.util.HibernateUtil;
+import jakarta.persistence.PersistenceException;
 import lombok.NoArgsConstructor;
 import org.hibernate.Session;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -14,50 +19,102 @@ import java.util.List;
 public class MatchDao {
 
     public void save(Matches match) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        session.persist(match);
-        log.info("save match : {}", match);
-        session.getTransaction().commit();
+        Transaction tx = null;
+
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            session.persist(match);
+            log.info("save match : {}", match);
+            tx.commit();
+        } catch (PersistenceException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            throw new DaoException("Не удалось сохранить матч: " + match.toString());
+        }
     }
 
     public List<Matches> getMatchesByPlayer(Players player, Integer currentPage, Integer limitPerPage) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query<Matches> query = session.createQuery("from Matches where player1 = :player or player2 = :player", Matches.class);
-        query.setFirstResult((currentPage - 1) * limitPerPage);
-        query.setMaxResults(limitPerPage);
-        List<Matches> matchesList = query.setParameter("player", player).getResultList();
-        session.getTransaction().commit();
+        List<Matches> matchesList = new ArrayList<>();
+        Transaction tx = null;
+
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            Query<Matches> query = session.createQuery("from Matches where player1 = :player or player2 = :player", Matches.class);
+            query.setFirstResult((currentPage - 1) * limitPerPage);
+            query.setMaxResults(limitPerPage);
+            matchesList = query.setParameter("player", player).getResultList();
+            tx.commit();
+        } catch (PersistenceException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("getMatchesByPlayer : {}", e.getMessage());
+        }
+
         return matchesList;
     }
 
     public List<Matches> getMatches(Integer currentPage, Integer limitPerPage) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query<Matches> query = session.createQuery("from Matches", Matches.class);
-        query.setFirstResult((currentPage - 1) * limitPerPage);
-        query.setMaxResults(limitPerPage);
-        List<Matches> matchesList = query.getResultList();
-        session.getTransaction().commit();
+        List<Matches> matchesList = new ArrayList<>();
+        Transaction tx = null;
+
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            Query<Matches> query = session.createQuery("from Matches", Matches.class);
+            query.setFirstResult((currentPage - 1) * limitPerPage);
+            query.setMaxResults(limitPerPage);
+            matchesList = query.getResultList();
+            tx.commit();
+        } catch (PersistenceException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("getMatches : {}", e.getMessage());
+        }
+
         return matchesList;
     }
 
     public Long getMatchesCount() {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query<Long> query = session.createQuery("select count(*) from Matches", Long.class);
-        Long count = query.getSingleResult();
-        session.getTransaction().commit();
+        Transaction tx = null;
+        Long count = 0L;
+
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            Query<Long> query = session.createQuery("select count(*) from Matches", Long.class);
+            count = query.getSingleResult();
+            tx.commit();
+        } catch (PersistenceException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("getMatchesCount : {}", e.getMessage());
+        }
         return count;
     }
 
     public Long getMatchesCountByPlayer(Players player) {
-        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-        Query<Long> query = session.createQuery("select count(*) from Matches where player1 = :player or player2 = :player", Long.class);
-        Long count = query.setParameter("player", player).getSingleResult();
-        session.getTransaction().commit();
+        Transaction tx = null;
+        Long count = 0L;
+
+        try {
+            Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+            tx = session.beginTransaction();
+            Query<Long> query = session.createQuery("select count(*) from Matches where player1 = :player or player2 = :player", Long.class);
+            count = query.setParameter("player", player).getSingleResult();
+            tx.commit();
+        } catch (PersistenceException e) {
+            if (tx != null) {
+                tx.rollback();
+            }
+            log.error("getMatchesCountByPlayer : {}", e.getMessage());
+        }
+
         return count;
     }
 }
