6a72a5ed792bea2168d426219d32a5c6b5166cde
Niker2023 <125988361+Niker2023@users.noreply.github.com>
2025-04-08 15:18:05 +0300

    refactor: divide the large method into several

--- src/main/java/com/project/service/MatchCreationService.java
+++ src/main/java/com/project/service/MatchCreationService.java
@@ -8,19 +8,30 @@ import java.util.UUID;
 
 public class MatchCreationService {
 
-    public UUID createMatch(String player1, String player2) {
+    public UUID createMatch(String player1Name, String player2Name) {
 
-        ValidationUtil.namesValidation(player1, player2);
-
-        var playersPersistenceService = new PlayersPersistenceService();
-
-        PlayerDto player1Dto = playersPersistenceService.save(player1);
-        PlayerDto player2Dto = playersPersistenceService.save(player2);
+        checkNames(player1Name, player2Name);
+        
+        PlayerDto player1Dto = savePlayerToDB(player1Name);
+        PlayerDto player2Dto = savePlayerToDB(player2Name);
 
         Score matchScore = new Score(player1Dto, player2Dto);
 
         OngoingMatchesService ongoingMatchesService = new OngoingMatchesService();
 
         return ongoingMatchesService.addMatch(matchScore);
+    }
+
+
+    private PlayerDto savePlayerToDB(String playerName) {
+        var playersPersistenceService = new PlayersPersistenceService();
+        return playersPersistenceService.save(playerName);
+    }
+
+
+    private void checkNames(String name1, String name2) {
+        ValidationUtil.isName(name1);
+        ValidationUtil.isName(name2);
+        ValidationUtil.namesComparing(name1, name2);
     }
 }
